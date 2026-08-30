b57e874b5929289b84e917944a9b3dfe707e623d
koltmk1 <igother770@gmail.com>
2026-08-30 17:29:18 -0300

    Progresso
    .

---
+++ inicial.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+
+    link.addEventListener("click", function (e) {
+
+        e.preventDefault();
+
+        const destino = document.querySelector(this.getAttribute("href"));
+
+        if (destino) {
+            destino.scrollIntoView({
+                behavior: "smooth"
+            });
+        }
+
+    });
+
+});
