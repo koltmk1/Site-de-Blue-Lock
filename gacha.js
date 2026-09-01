55eacd2a69ad7f782d3553228ca355072ddf1356
vnyzs <vinnyvieiradasilva12@gmail.com>
2026-09-01 09:03:42 -0300

    .
    .

---
+++ gacha.js
@@ -0,0 +1,7 @@
+const personagens = {
+  comum: [Isagi, Bachira, Chigiri, Igarashi, Niko],
+  raro: [Reo, Tokimitsu, Zantetsu, Otoya],
+  epico: [Nagi, Yukimiya, Barou, Aryu],
+  lendario: [Rin, Kiyora, ],
+  newGen: [Sae, Lorenzo, Hugo, Loki,]
+}
