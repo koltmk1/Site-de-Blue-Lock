964b3fadac97d8b50bcf1a81997185641982c146
vnyzs <vinnyvieiradasilva12@gmail.com>
2026-09-01 08:45:03 -0300

    .
    .

--- inicial.js
+++ inicial.js
@@ -16,28 +16,88 @@ document.querySelectorAll('a[href^="#"]').forEach(link => {
 
 });
 
+const raridades = {
+    comum: ["Isagi", "Kunigami", "Chigiri"],
+    raro: ["Bachira", "Nagi"],
+    epico: ["Rin", "Barou"],
+    lendario: ["Shidou"],
+    newgen: ["Loki", "Hugo", "Bunny"]
+};
+
 const personagens = [
     {
-        nome: "Isagi Yoichi",
-        imagem: "Primeira fase.jpg",
-        raridade: "Raro"
+        nome: "Isagi",
+        raridade: "Comum",
+        chance: 60,
+        imagem: "./img/Primeira fase.jpg"
     },
-
     {
-        nome: "Meguru Bachira",
-        imagem: "Linguinha.jpg",
-        raridade: "Raro"
+        nome: "Bachira",
+        raridade: "Raro",
+        chance: 25,
+        imagem: "./img/Linguinha.jpg"
     },
-
     {
-        nome: "Nagi Seishiro",
-        imagem: "Gênio Preguiçoso.jpg",
-        raridade: "Épico"
+        nome: "Rin",
+        raridade: "Épico",
+        chance: 10,
+        imagem: "./img/Corte de água.jpg"
     },
-
     {
-        nome: "Hyoma Chigiri",
-        imagem: "Pantera Vermelha.jpg",
-        raridade: "Raro"
+        nome: "Shidou",
+        raridade: "Lendário",
+        chance: 4,
+        imagem: "./img/shidou.webp"
+    },
+    {
+        nome: "Loki",
+        raridade: "New Gen",
+        chance: 1,
+        imagem: "./img/loki pro site.jpg"
     }
 ];
+
+function sortearPersonagem() {
+    let numero = Math.random() * 100;
+    let acumulado = 0;
+
+    for (let personagem of personagens) {
+        acumulado += personagem.chance;
+
+        if (numero <= acumulado) {
+            return personagem;
+        }
+    }
+}
+
+const botao = document.getElementById("girar");
+
+botao.addEventListener("click", revelar);
+
+function revelar() {
+    const personagem = sortearPersonagem();
+
+    document.getElementById("imgPersonagem").src = personagem.imagem;
+    document.getElementById("nomePersonagem").textContent = personagem.nome;
+    document.getElementById("raridade").textContent = personagem.raridade;
+}
+
+function revelar() {
+
+    const img = document.getElementById("imgPersonagem");
+
+    img.classList.add("abrindo");
+
+    setTimeout(() => {
+
+        const personagem = sortearPersonagem();
+
+        img.src = personagem.imagem;
+        document.getElementById("nomePersonagem").textContent = personagem.nome;
+        document.getElementById("raridade").textContent = personagem.raridade;
+
+        img.classList.remove("abrindo");
+
+    }, 2000);
+
+}
